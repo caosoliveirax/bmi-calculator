c89957932d5477d918fbba775287bd64cfb60f33
Carlos <kaduchaos18@gmail.com>
2025-04-15 14:11:44 -0300

    chore: add Bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,17 @@ function imageCompressor() {
 };
 
 function compileSass() {
-    return gulp.src('./src/styles/main.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({style: 'compressed'})).on('error', sass.logError)
-    .pipe(sourcemaps.write('./maps'))
+    return gulp.src('./src/styles/main.scss') 
+        .pipe(sourcemaps.init())
+        .pipe(sass({ style: 'compressed' }).on('error', sass.logError)) 
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist/styles'));
+}
+
+function copyBootstrap() {
+    return gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
     .pipe(gulp.dest('./dist/styles'));
-};
+}
 
 function jsCompressor() {
     return gulp.src('./src/scripts/*.js')
@@ -28,4 +33,5 @@ exports.default = function() {
     gulp.watch('./src/images/*', {ignoreInitial: false}, gulp.parallel(imageCompressor));
     gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.series(compileSass));
     gulp.watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.series(jsCompressor));
+    gulp.series(copyBootstrap)();
 };
